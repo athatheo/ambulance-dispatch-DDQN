9b843565a6ad2632329c66133cbe666d5b4c53df
Francesca Drummer <franci@Francescas-MacBook-Pro.local>
2021-09-14 16:13:28 +0200

    Import data

--- Environment.py
+++ Environment.py
@@ -10,6 +10,10 @@ class environment:
         self.postcode_dic = {}  # for each region all available postcodes
         self.coverage_lst = []  # list of dictionaries; each dictionary contains the coverage time of one region
         self.accidents = {} # number of total accidents per region
+        self.bases = {} # dictionary with all ambulance bases for each region
+        self.hospitals = {} # dictionary with region as keys and hospital postcodes as values
+        self.nr_postcodes = {} # records number of postcodes per region
+        self.nr_ambulances = {} # records number of ambulances per region
 
         print("Initialisation complete")
 
@@ -29,7 +33,6 @@ class environment:
             coverage_df = pd.read_excel('Data/coverage{}.xlsx'.format(i), header = None)
 
             self.coverage_lst.append(coverage_df.to_dict('list'))
-            print(self.coverage_lst)
 
             with open('Data/population{}.txt'.format(i)) as f:
                 lines = f.readlines()
@@ -45,28 +48,48 @@ class environment:
 
             self.postcode_dic.update({i: postcode_lst})
             self.pop_dic.update({i: pop_lst})
-            print(self.postcode_dic)
-            print(self.pop_dic)
+        print(self.postcode_dic)
+        print(self.pop_dic)
 
         # number of registered accidents per region
-        with open('Data/DataAllRegions.txt'.format(i)) as f:
+        with open('Data/DataAllRegions.txt') as f:
             lines = f.readlines()
 
 
-        for i, line in enumerate(lines[1:]):
+        for line in lines[1:]:
             temp = re.findall(r'\d+', line)
             values = list(map(int, temp))
             self.accidents.update({values[0]: values[1]})
         print("Accidents: {}".format(self.accidents))
 
         # location of bases and number of ambulances per base
-        with open('Data/xMEXCLP.txt'.format(i)) as f:
+        with open('Data/xMEXCLP_all.txt') as f:
+            lines = f.readlines()
+
+        for i, line in enumerate(lines):
+            res = re.split(',|;', line)
+            temp = [re.findall(r'\d+', s) for s in res if re.findall(r'\d+', s) != []]
+            reg_bases = {l[0]: l[1] for l in temp}
+            self.bases.update({i + 1: reg_bases})
+        print(self.bases)
+
+        # retrieve the postcode locations for each hospital
+        # retrieve the postcode locations for each hospital
+        with open('Data/Hospitals.txt') as f:
             lines = f.readlines()
 
         for line in lines:
-            res = re.split(',  |;|', line)
+            res = re.split(',|:', line.strip('\n'))
+            self.hospitals.update({int(res[0]): list(map(int, res[1:]))})
+        print(self.hospitals)
 
-
+        # extract region sizes and ambulances per region
+        ambuRegion = pd.read_excel('Data/NumberPCAmbuRegion.xlsx')
+        ambuRegion = ambuRegion.set_index('Region')
+        self.nr_postcodes = ambuRegion['Postal codes'].to_dict()
+        self.nr_ambulances = ambuRegion['ambulances'].to_dict()
+        print(self.nr_ambulances)
+        print(self.nr_postcodes)
 
     def calculate_ttt(self):
         """
@@ -74,3 +97,8 @@ class environment:
         :return: total travel time in ms
         """
 
+    def sample_accidents(self):
+        """
+        sample accidents uniformly over time
+        :return:
+        """
