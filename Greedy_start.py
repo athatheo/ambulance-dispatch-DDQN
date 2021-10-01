bceaa2e3aa7fecbeefcbafbf6b22612513fe4d92
vaan95 <51932407+vaan95@users.noreply.github.com>
2021-10-01 12:32:33 +0200

    Greedy_start.py

---
+++ Greedy_start.py
@@ -0,0 +1,30 @@
+"""
+distance(ambulance_loc, accident_loc)
+
+input: zipcodes ambulances, accident
+else: total_distance() -> when is ambulance available
+      state
+
+"""
+
+
+def state_available(self,region, availability)
+base_list= self.bases(region)
+available_bases=[]
+#int counter= total_distance            #count down counter
+for i, base in enumerate(base_list):
+    #available_bases.append(int base))       #list of bases per region
+    if base_list -availability[i]>0
+        available_bases.append(int(base))
+    
+    #availability [base1_amount, base2_amount... ]
+    availability[i]=availability[i]-1
+
+
+# read from sample-Accident where/when accident happend
+# start counter       #count up
+
+#counter needed to  +i -> when back to base     
+
+total_time=
+
